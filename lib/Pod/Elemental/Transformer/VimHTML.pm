831871bfebbfdc329df50835011d5f907fb1b5e0
Ricardo Signes <rjbs@cpan.org>
2009-12-11 18:47:21 -0500

    docs

--- lib/Pod/Elemental/Transformer/VimHTML.pm
+++ lib/Pod/Elemental/Transformer/VimHTML.pm
@@ -3,6 +3,34 @@ use Moose;
 with 'Pod::Elemental::Transformer::SynHi';
 # ABSTRACT: convert "=begin vim" regions to colorized XHTML with Vim
 
+=head1 DESCRIPTION
+
+This transformer looks for regions like this:
+
+  =begin vim lisp
+
+    (map (stuff (lisp-has-lots-of '(,parens right))))
+
+  =end vim
+
+...into syntax-highlighted HTML that I can't really usefully represent here.
+It uses L<Text::VimColor>, so you can read more about the kind of HTML it will
+produce, there.  The parameter after "=begin vim" is used as the filetype.
+
+This form is also accepted:
+
+  #!vim lisp
+  (map (stuff (lisp-has-lots-of '(,parens right))))
+
+In the above example, the shebang-like line will be stripped.  The filetype
+parameter is I<mandatory>.
+
+B<Achtung!>  Two leading spaces are stripped from each line of the content to
+be highlighted.  This behavior may change and become more configurable in the
+future.
+
+=cut
+
 use Text::VimColor;
 
 sub build_html {
@@ -10,7 +38,6 @@ sub build_html {
   my $string = $arg->{content};
   my $syntax = $arg->{syntax};
 
-  1 while chomp $string;
   $string =~ s/^  //gms;
 
   my $vim = Text::VimColor->new(
@@ -34,6 +61,13 @@ sub synhi_params_for_para {
       syntax  => $para->content,
       content => $para->children->[0]->as_pod_string,
     };
+  } elsif ($para->isa('Pod::Elemental::Element::Pod5::Verbatim')) {
+    my $content = $para->content;
+    return unless $content =~ s/\A\s*#!vim\s+(\S+)\n+//gsm;
+    return {
+      content => $content,
+      syntax  => $1,
+    }
   }
 
   return;
